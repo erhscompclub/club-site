99f2119e89241f7f3cd747a7e58ba4f77466f2a7
ERHS Computer Club <erhscompclub@users.noreply.github.com>
2016-04-27 15:22:46 -0400

    Added html lesson link

--- linkdatabase.js
+++ linkdatabase.js
@@ -1,8 +1,8 @@
 var linkData=[
 	{
 		"name":"Lessons",
-		"links":["Console","Javascript","Clicking","GitHub"],
-		"hrefs":["lesson.html?lesson=Console","lesson.html?lesson=Javascript","lesson.html?lesson=Clicking","lesson.html?lesson=GitHub"]
+		"links":["Console","Javascript","Clicking","GitHub","HTML"],
+		"hrefs":["lesson.html?lesson=Console","lesson.html?lesson=Javascript","lesson.html?lesson=Clicking","lesson.html?lesson=GitHub","lesson.html?lesson=html"]
 	},
 	{
 		"name":"Sample Games",
