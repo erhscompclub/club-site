54ebe3a8d8b5889137fb4485b302f05e0161cea0
ERHS Computer Club <erhscompclub@users.noreply.github.com>
2016-04-27 20:31:18 -0400

    Added new lore news

--- news.js
+++ news.js
@@ -1,4 +1,5 @@
 var news=[
+	"A new chapter in the Saga of Computer Club has been unlocked! Check out the Archives to read it.",
 	"Now that my code repository nightmare has (finally) ended, we can start working on our games. I will start my own group and work with those who come and go, or who don't already have a team. Start programming your games, and commit any changes to your GitHub repository. If you have an issue, either email me or we can address it in computer club.",
 	"Two new lessons have (finally) been added to the website: Click handling in Javascript and the Console. If you want to start working on your games, laying out the barebones HTML tags would be a good idea. Good luck with your programs!",
 	"The second (and sadly, the last) Epic Game Night of the year was also a success! (Kek, just shows you how long ago I updated this website) We are looking forward to see that amount of ticket sales again next year!",
