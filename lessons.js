5c5d6413fdb09316426f03d8528c5e9b55cfc60a
ERHS Computer Club <erhscompclub@users.noreply.github.com>
2016-05-03 15:19:38 -0400

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -101,7 +101,7 @@ var lessons=[
 		"text":["<heading><u>HTML</u></heading></br></br>HTML is the backbone of every webpage on the internet. It will provide you with the tools to create the bare bones of a website. ",
 		"HTML is a relatively simple language, and can be augmented by two other languages: JavaScript and CSS. In html, you have something called <i>tags</i>. ",
 		"Tags are the basic building blocks of HTML. Individual tags can even be customized using <i>attributes</i>. Here is an example of a complete tag:",
-		"</br>"+tab(1)+"<xmp><font color=\"red\">Hello!</font></xmp></code></br><code>font</code> is the tag. <code>color</code> is an attribute.</br>",
+		"</br>"+tab(1)+"<xmp><font color=\"red\">Hello!</font></xmp></code></br><code>font</code> is the tag. <code>color</code> is an attribute.</br></br>",
 		"<heading><u>Common HTML Tags</u></heading></br></br><xmp>•<html> - Every other tag in your webpage must come between <html> and </html>.</xmp>",
 		"<xmp>•<title></title> - whatever you type between these tags will be set as the webpage's tab name.</xmp><xmp>•<body> - most of your html code will be within a <body> tag. <body> represents the background of your webpage.</xmp>",
 		"<xmp>•<p> - sets apart text within the tag as a single paragraph.</xmp><xmp>•<font> - uses attributes to change some text's color, font size, and decoration.</xmp><xmp>•</br> - </br> doesn't require a <br> tag. It tells the webpage to skip down to the next time.</xmp>",
