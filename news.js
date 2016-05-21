532ca7b315e4c04f43b2d10ee75761f87788dadf
ERHS Computer Club <erhscompclub@users.noreply.github.com>
2016-05-20 20:03:57 -0400

    Added news

--- news.js
+++ news.js
@@ -1,11 +1,9 @@
 var news=[
+	"Attention club members! This coming Wednesday (the 25th) is the last meeting for the 2016 school year :(</br>But then again...we do get pizza!!! Remember to being $3 for the food.",
+	"Ha! I won the Pizza Civil War! In yo face Dagm!!! (Jk bro)",
 	"I added a new chapter to the JavaScript lesson: Classes. Classes are essential to just about every game you program, so be sure to read through it.",
 	"A new chapter in the Saga of Computer Club has been unlocked! Check out the Archives to read it.",
 	"Now that my code repository nightmare has (finally) ended, we can start working on our games. I will start my own group and work with those who come and go, or who don't already have a team. Start programming your games, and commit any changes to your GitHub repository. If you have an issue, either email me or we can address it in computer club.",
 	"Two new lessons have (finally) been added to the website: Click handling in Javascript and the Console. If you want to start working on your games, laying out the barebones HTML tags would be a good idea. Good luck with your programs!",
-	"The second (and sadly, the last) Epic Game Night of the year was also a success! (Kek, just shows you how long ago I updated this website) We are looking forward to see that amount of ticket sales again next year!",
-	"Epic Game Night was a success!! Thank you to all who could attend. For our next meeting, we will start on Unity. I'm not entirely sure what will be done with it, but Dagm will be teaching. The Unity Global Game Jam is coming up soon, which will give you all some good experience. After that, we will hopefully finish up your Javascript projects.",
-	"Here\'s the demo version of our new site. Programming lessons will be posted soon.",
-	"Well, I\'m currently working on the computer club website, so I guess I\'ll put that up here...</br>How do you like it? ^-^",
-	"This is the news section of the site. Links (to lessons and such) can be found to the left."
+	"The second (and sadly, the last) Epic Game Night of the year was also a success! (Kek, just shows you how long ago I updated this website) We are looking forward to see that amount of ticket sales again next year!"
 ]
