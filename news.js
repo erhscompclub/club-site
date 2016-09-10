b4f7a018400f7232b3ab9185f45984c5285dd999
ERHS Computer Club <erhscompclub@users.noreply.github.com>
2016-09-10 12:07:23 -0400

    Update news.js

--- news.js
+++ news.js
@@ -1,11 +1,11 @@
 var news=[
+	"If there is any specific topic anyone wants to learn about, I suggest you email me (Alex Lugo) about it. You can find a link to my pgcps email at the bottom of the page.",
+	"New lessons coming soon. The delay is because I may rework the structure of my lesson query code. Simplicity is best, right? Also, sorry about cancelling last Wednesday's meeting (on 9/7/2016). Mr.Pruszko was out that day. We will have a meeting this Wednesday, however, so put it on your schedule.",
 	"There's only 6 days left until school starts (prepare yo self fools) and I've finally managed to upload my first lesson of the summer :D</br>Also since this is the last year for Dagm and I, we'll need to train the next generation of Computer Club leaders for the year after. Consider it a prophecy. Also, I'm pimping my <a href=\"http:alugocp.github.io/resume\"><u>online resume</u></a> right here.",
 	"I have an announcement: I have finally overcome my extreme laziness and am now working on new lessons for the website. Also, I added some more content to pre-existing lessons (mainly the concepts lesson). I plan to get a simple physics, command line basics, and (maybe) a java lesson out this summer. Or maybe a low-level programming language lesson? Idk, you'll have to see ;D",
 	"End of the year message:</br>Thank you all for a fun year of Computer Club. That banquet was pretty lit.</br>Sorry we never got to complete our games, we were a bit tied up in the Phil Wars. With the club website, however, Computer Club will come back better than ever next year. Here's to a new Golden Age!",
 	"The entrance fee to the end of the year banquet has been changed to $5, on account of all the food we ordered. Also, you must be a member of computer club to attend. See you all tomorrow.",
 	"Attention club members! This coming Wednesday (the 25th) is the last meeting for the 2016 school year :(</br>But then again...we do get pizza!!! Remember to being $3 for the food.",
 	"New features:</br> 1.) A new chapter in the storyline</br> 2.) Added new languages to the enyclopedia",
-	"Ha! I won the Pizza Civil War! In yo face Dagm!!! (Jk bro)",
-	"I added a new chapter to the JavaScript lesson: Classes. Classes are essential to just about every game you program, so be sure to read through it.",
-	"A new chapter in the Saga of Computer Club has been unlocked! Check out the Archives to read it.",
+	"Ha! I won the Pizza Civil War! In yo face Dagm!!! (Jk bro)"
 ]
