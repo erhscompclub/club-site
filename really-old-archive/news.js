7a90fbbce6559b7415ee590bee67f6c58024be8c
erhscompclub <alugocp@gmail.com>
2017-09-27 21:10:46 -0400

    Javascript is nice, but it gets messy

---
+++ really-old-archive/news.js
@@ -0,0 +1,17 @@
+var news=[
+	"For those of you who didn't get the email, here's the link to <a href=\"https://docs.google.com/forms/d/1AehgBFPrBp6H01uH4zs2RziVihJ8YhntqKjFIKjQ30E/edit\">Epic Game Night's volunteer survey</a>",
+	"What do you guys think about doing an end-of-the-year project? The entire club would work on it. It would either be a social media website, a desktop environment, a chat app, or a programming language.",
+	"Okay, the website's lesson system is completely re-done (well, there's one little thing I may want to fix, but I can do that later). Prepare yourselves for some dank new lessons.",
+	"The lessons system on the club website is halfway done with its revamp. Once it's complete I'll post new lessons faster than ever.",
+	"<a href=\"http://w3schools.com\">w3schools</a> has good HTML reference for those of you that want to experiment with it.",
+	"(9/11/2016) Let it be written in the Book of Harambe, from the Bible of the Twerk God, that on this day Dagm Sirak was dubbed the Meme King.",
+	"If there is any specific topic anyone wants to learn about, I suggest you email me (Alex Lugo) about it. You can find a link to my pgcps email at the bottom of the page.",
+	"New lessons coming soon. The delay is because I may rework the structure of my lesson query code. Simplicity is best, right? Also, sorry about cancelling last Wednesday's meeting (on 9/7/2016). Mr.Pruszko was out that day. We will have a meeting this Wednesday, however, so put it on your schedule.",
+	"There's only 6 days left until school starts (prepare yo self fools) and I've finally managed to upload my first lesson of the summer :D</br>Also since this is the last year for Dagm and I, we'll need to train the next generation of Computer Club leaders for the year after. Consider it a prophecy. Also, I'm pimping my <a href=\"http:alugocp.github.io/resume\"><u>online resume</u></a> right here.",
+	"I have an announcement: I have finally overcome my extreme laziness and am now working on new lessons for the website. Also, I added some more content to pre-existing lessons (mainly the concepts lesson). I plan to get a simple physics, command line basics, and (maybe) a java lesson out this summer. Or maybe a low-level programming language lesson? Idk, you'll have to see ;D",
+	"End of the year message:</br>Thank you all for a fun year of Computer Club. That banquet was pretty lit.</br>Sorry we never got to complete our games, we were a bit tied up in the Phil Wars. With the club website, however, Computer Club will come back better than ever next year. Here's to a new Golden Age!",
+	"The entrance fee to the end of the year banquet has been changed to $5, on account of all the food we ordered. Also, you must be a member of computer club to attend. See you all tomorrow.",
+	"Attention club members! This coming Wednesday (the 25th) is the last meeting for the 2016 school year :(</br>But then again...we do get pizza!!! Remember to being $3 for the food.",
+	"New features:</br> 1.) A new chapter in the storyline</br> 2.) Added new languages to the enyclopedia",
+	"Ha! I won the Pizza Civil War! In yo face Dagm!!! (Jk bro)"
+]
